b56b2d4e04a047858d444287a84c8afd85015f94
Javier Lavandeira <javi@lavandeira.net>
2017-04-12 00:00:34 +0900

    Removed the admin routes from the main module and replaced them with an import.

--- routes/admin/page_admin_news.py
+++ routes/admin/page_admin_news.py
@@ -1,6 +1,6 @@
 from flask import abort, render_template
 from __main__ import app
-from . import NewsItem, User
+from models import NewsItem, User
 
 @app.route('/admin/news', methods=['GET'])
 def page_admin_news():
