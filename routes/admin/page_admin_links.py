742f1c080f1c784d4b1592ef4274afab2f24912c
Javier Lavandeira <javi@lavandeira.net>
2017-04-16 16:01:10 +0900

    Added support for listing, adding and editing links (ExternalLink model) from the admin interface.

--- routes/admin/page_admin_links.py
+++ routes/admin/page_admin_links.py
@@ -3,7 +3,7 @@ from __main__ import app
 from models import ExternalLink, User
 
 @app.route('/admin/links', methods=['GET'])
-def page_admin_news():
+def page_admin_links():
 
 	# Get the signed in User (if there's one), or None
 	user = User.get_signed_in_user()
