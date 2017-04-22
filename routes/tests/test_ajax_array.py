8f3a17867ee8277731e83e610fa444de64637ffa
Javier Lavandeira <javi@lavandeira.net>
2017-04-22 19:21:17 +0900

    Added new route to get information about ExternalLinks in JSON format via an AJAX request from the admin interface.

--- routes/tests/test_ajax_array.py
+++ routes/tests/test_ajax_array.py
@@ -1,16 +1,14 @@
 import json
-from flask import jsonify, request
+from flask import jsonify, request, json
 from utils import log_form_vars
 from __main__ import app
 
 @app.route('/test/ajax/array', methods=['POST'])
 def test_ajax_array():
 
-	app.logger.info("*** DEBUG: Length of the 'json' array is %s" % len(request.json))
-
 	log_form_vars(request.json)
 
-	for var in request.json['items']:
+	for var in request.json.loads('items'):
 		app.logger.info("*** DEBUG: Related news has ExternalLink #%s" % var)
 
 	return jsonify(request.json)
