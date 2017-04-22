f779a7fdf5a3c02ddd0133a370a68050a9d643b4
Javier Lavandeira <javi@lavandeira.net>
2017-04-22 19:17:42 +0900

    Added new route to get information about ExternalLinks in JSON format via an AJAX request from the admin interface.

--- routes/tests/test_ajax_array.py
+++ routes/tests/test_ajax_array.py
@@ -6,11 +6,11 @@ from __main__ import app
 @app.route('/test/ajax/array', methods=['POST'])
 def test_ajax_array():
 
-	app.logger.info("*** DEBUG: Length of the items' array with getlist is %s" % len(request.form.getlist('items')))
+	app.logger.info("*** DEBUG: Length of the 'json' array is %s" % len(request.json))
 
-	log_form_vars(request.form)
+	log_form_vars(request.json)
 
-	for var in request.form.getlist('items'):
+	for var in request.json['items']:
 		app.logger.info("*** DEBUG: Related news has ExternalLink #%s" % var)
 
-	return jsonify(request.form.getlist('items'))
+	return jsonify(request.json)
