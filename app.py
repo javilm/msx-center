bf0bd1c39b66eabe769d702de604887bcf1c13c3
Javier Lavandeira <javi@lavandeira.net>
2017-04-27 19:45:18 +0900

    Implemented listing, adding and editing articles in the admin panel.

--- app.py
+++ app.py
@@ -54,6 +54,7 @@ from routes.account import *
 from routes.lounges import *
 from routes.member import *
 from routes.news import *
+from routes.articles import *
 from routes.admin import *
 # from routes.tests import *
 
