20b33178e1c015cd0e408abea067f8f1fd013854
Javier Lavandeira <javi@lavandeira.net>
2017-04-27 19:48:31 +0900

    Implemented listing, adding and editing articles in the admin panel.

--- routes/news/page_news.py
+++ routes/news/page_news.py
@@ -12,6 +12,7 @@ def page_news():
 	template_options['user'] = User.get_signed_in_user()
 
 	# Get the news items
-	template_options['items'] = NewsItem.query.order_by(NewsItem.date_published.desc()).limit(5)
+#	template_options['items'] = NewsItem.query.order_by(NewsItem.date_published.desc()).limit(5)
+	template_options['items'] = NewsItem.query.all()
 
 	return render_template('news/news-list.html', **template_options)
