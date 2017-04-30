25f1326c90cfebb3c03bf04f70eec00c37cdfac5
Javier Lavandeira <javi@lavandeira.net>
2017-04-30 12:27:54 +0900

    Modifying nav bar to dynamically generate dropdown with list of article series.

--- routes/articles/page_series_articles.py
+++ routes/articles/page_series_articles.py
@@ -12,10 +12,10 @@ def page_series_articles(series_id, slug):
 	template_options['user'] = User.get_signed_in_user()
 
 	# Get the details for this series
-	tmeplate_options['series'] = ArticleSeries.query.filter_by(id=series_id).first()
+	template_options['series'] = ArticleSeries.query.filter_by(id=series_id).first()
 
 	# Get all the articles in this series
-	tmeplate_options['articles'] = Article.query.filter_by(series_id=series_id).order_by(Article.date_published).all()
+	template_options['articles'] = Article.query.filter_by(series_id=series_id).order_by(Article.date_published).all()
 
 	# XXX Create this template
 	return render_template('articles/articles.html', **template_options)
