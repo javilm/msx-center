63458280bbda9e0c7acd7da05135ba3689377d48
Javier Lavandeira <javi@lavandeira.net>
2017-04-30 14:33:05 +0900

    Added template and route for list of articles in a series.

--- routes/articles/__init__.py
+++ routes/articles/__init__.py
@@ -1,7 +1,9 @@
 from page_articles import page_articles
 from page_series_articles import page_series_articles
+from page_articles_detail import page_articles_detail
 
 __all__ = [
 	"page_articles",
-	"page_series_articles"
+	"page_series_articles",
+	"page_articles_detail"
 ]
