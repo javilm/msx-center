7b3eb754dc5ecc91ea63c01af3aa5ce0c4f645d7
Javier Lavandeira <javi@lavandeira.net>
2017-05-01 17:23:25 +0900

    Updated several routes to use a “template_options” dictionary when passing arguments to templates, instead of passing each keyword argument one by one in the render_template() call.

--- routes/articles/page_articles_detail.py
+++ routes/articles/page_articles_detail.py
@@ -8,13 +8,18 @@ def page_articles_detail(article_id, slug):
 
 	template_options = {}
 	template_options['user'] = User.get_signed_in_user()
-	template_options['navbar_series'] = ArticleSeries.query.order_by(ArticleSeries.priority).all()
-	
-	# Get the details for this series
-	template_options['series'] = ArticleSeries.query.filter_by(id=series_id).first()
 
-	# Get all the articles in this series
-	template_options['article'] = Article.query.filter_by(id=article_id).first()
+	# This article
+	article = Article.query.filter_by(id=article_id).first()
+	if article is None:
+		abort(404)
+
+	template_options['article'] = article
+	template_options['navbar_series'] = ArticleSeries.query.order_by(ArticleSeries.priority).all()	# all the series, for the navbar
+	
+	# the series this article belongs to	
+	template_options['series'] = ArticleSeries.query.filter_by(id=article.series_id).first()
+
 
 	# XXX Create this template
 	return render_template('articles/article-detail.html', **template_options)
