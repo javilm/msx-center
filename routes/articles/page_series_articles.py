0af32720a2e46495b114aaeda0f172996703d467
Javi Lavandeira <javi@lavandeira.net>
2017-05-24 23:50:42 +0900

    Replaced the hardcoded ad in the sidebar with a real Google Ads unit.

--- routes/articles/page_series_articles.py
+++ routes/articles/page_series_articles.py
@@ -11,9 +11,9 @@ def page_series_articles(series_id, slug):
 	# Get the signed in User (if there's one), or None
 	template_options['user'] = User.get_signed_in_user()
 	template_options['active'] = 'articles'
-	template_options['navbar_series'] = ArticleSeries.query.order_by(ArticleSeries.priority).all()
+	template_options['navbar_series'] = ArticleSeries.list_for_navbar()
 
-	template_options['series'] = ArticleSeries.list_for_navbar()
+	template_options['series'] = ArticleSeries.query.filter_by(id=series_id).first()
 
 	# Get all the articles in this series
 	template_options['articles'] = Article.query.filter_by(series_id=series_id, is_draft_en=False, is_published=True, is_hidden=False).order_by(Article.date_published).all()
