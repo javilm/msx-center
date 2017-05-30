d43b5cda8719931ea04cd3e9db8878c39b4c0954
Javi Lavandeira <javi@lavandeira.net>
2017-05-30 19:45:49 +0900

    Added breadcrumbs to a few pages. Other minor changes.

--- routes/articles/page_series_articles.py
+++ routes/articles/page_series_articles.py
@@ -1,6 +1,7 @@
 from __main__ import app
 from flask import url_for, render_template, session
 from models import User, ArticleSeries, Article
+from sqlalchemy import desc
 
 @app.route('/series/<int:series_id>/<string:slug>', methods=['GET'])
 def page_series_articles(series_id, slug):
@@ -17,5 +18,8 @@ def page_series_articles(series_id, slug):
 
 	# Get all the articles in this series
 	template_options['articles'] = Article.query.filter_by(series_id=series_id, is_draft_en=False, is_published=True, is_hidden=False).order_by(Article.date_published).all()
+
+	# Sidebar's most popular articles
+	template_options['popular_articles'] = Article.query.filter_by(is_draft_en=False, is_published=True, is_hidden=False).order_by(desc(Article.num_views)).limit(10)
 
 	return render_template('articles/articles-series-detail.html', **template_options)
